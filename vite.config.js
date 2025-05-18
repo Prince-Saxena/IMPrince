48ab4d0c07a4d1ac711e16ea31fd6e16bb6db182
Prince Saxena <princesaxena202020@gmail.com>
2025-05-18 20:37:17 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "./",
+	base: "/Prince-Saxena.github.io/",
 	plugins: [tailwindcss(), react()],
 });
